405e6e90b35d664dd991c61793617ed805d5c9ac
Anatereza <mascarenhas.anatereza@gmail.com>
2021-02-18 20:11:45 +0100

    Hash => identifiant + Croix rouge ErrorMessage

--- client/src/components/ErrorMessage.js
+++ client/src/components/ErrorMessage.js
@@ -6,7 +6,7 @@ class ErrorMessage extends Component {
         return ( 
             <>
             <div>
-                <i class="fa fa-times-circle ct-red fa-3x" style={{marginBottom:"30px"}}></i>
+                {/*<i class="fa fa-times-circle ct-red fa-3x" style={{marginBottom:"30px"}}></i>*/}
                 <h2 className="ct-red">{this.props.message}</h2>
                 <h4>{this.props.sousMessage}</h4>
             </div>
