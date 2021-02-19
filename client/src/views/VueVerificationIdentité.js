2ff60b62335da1b16dfdbcb94a3b64abc9d9b687
MatthieuBougeard <matthieu.bougeard@hotmail.fr>
2021-02-19 19:10:44 +0100

    blabla verification ID

--- client/src/views/VueVerificationIdentité.js
+++ client/src/views/VueVerificationIdentité.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
-import ComponentVérificationID from "components/ComponentVérificationID"
+import ComponentVérificationID from "components/ComponentVérificationID";
+import image1 from "assets/img/ImagesHomeCitoyen/Image1.png";
+import image2 from "assets/img/ImagesHomeCitoyen/Image2.png";
+import image3 from "assets/img/ImagesHomeCitoyen/Image3.png";
+import image4 from "assets/img/ImagesHomeCitoyen/Image4.png";
+import imageAjouter from 'assets/img/IconesAccueils/Ajouter.png'
+ 
+
 import {
     Container,
     Row,
@@ -22,7 +29,7 @@ class VueVérificationIdentité extends Component {
             <Container style={{minHeight:"100vh"}}>
                 <Row style={{height:"100px"}}></Row>
                 <Row style={{height:"100%"}}>
-                    <Col md={{ size: 9, offset: 0 }}>
+                    <Col md={{ size: 8, offset: 0 }}>
                         <Row className="text-center" style={{paddingTop:"50px"}, {marginBottom:"50px"}}>
                             <h1 style={{color:"gray"}}>Vérifier une identité</h1>
                         </Row>
@@ -33,8 +40,28 @@ class VueVérificationIdentité extends Component {
                             <ComponentVérificationID defaultHash={this.state.paramHash && this.state.paramHash}></ComponentVérificationID>
                         </Row>
                     </Col>
-                    <Col style={{height:"100%"}}>
-                        <div>BLABLA</div>
+
+                    <Col className="no-gutter flex-container-col-left bloc-how-to" style={{marginLeft:"20px"}}>
+                        <h4>Comment ça marche ? </h4>
+                        <Row className="flex-container-left-center" > 
+                            <p className="round-numbers"><div style={{verticalAlign:"middle"}}>1</div></p>
+                            <div className="text-how-to">Je saisi la clé de sécurité</div>  
+                        </Row>
+                        <img className="img-verif-id" alt="..." style={{width:"250px"}} src={image1}/>
+                        <Row className="flex-container-left-center" >
+                            <p className="round-numbers"><div style={{verticalAlign:"middle"}}>2</div></p>
+                            <div className="text-how-to">Je lance la recherche</div>
+                        </Row>
+                        <img className="img-verif-id" alt="..." style={{width:"250px"}} src={image2}/>
+                        <Row className="flex-container-left-center" >
+                            <p className="round-numbers"><div style={{verticalAlign:"middle"}}>3</div></p>
+                            <div className="text-how-to">Les données sont certifiées</div>
+                        </Row>
+                        <img className="img-verif-id" alt="..." style={{width:"250px"}} src={image3}/>
+                        <Row className="flex-container-left-center" >
+                            <div style={{marginLeft:"70px"}} className="text-how-to">Sinon je suis averti.e</div>
+                        </Row>
+                        <img className="img-verif-id" alt="..." style={{width:"250px"}} src={image4}/>
                     </Col>
                 </Row>
             </Container>
