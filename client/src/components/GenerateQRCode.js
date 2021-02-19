0d1f9d6d39023f7f0023dba8e9dc61c81693aaba
MatthieuBougeard <matthieu.bougeard@hotmail.fr>
2021-02-19 08:34:04 +0100

    Modification du lien dans le QRCOde

--- client/src/components/GenerateQRCode.js
+++ client/src/components/GenerateQRCode.js
@@ -1,6 +1,6 @@
 function GenerateQRCode (_hash) {
 //TODO : Récupérer le hash dans le contexte courant
-    const hash = "http://localhost:3000/verification-id#" + _hash
+    const hash = "http://localhost:3000/verification-id?" + _hash
     const link = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
     const result = link + hash
 
