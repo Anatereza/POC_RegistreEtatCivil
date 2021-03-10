670a0beb04ed09e660e9d36b5df3a72505ee50a2
MatthieuBougeard <matthieu.bougeard@hotmail.fr>
2021-03-10 11:21:17 +0100

    Ajout décision de justice sur page accueil mairie

--- client/src/views/Mairie/AccueilMairie.js
+++ client/src/views/Mairie/AccueilMairie.js
@@ -4,18 +4,42 @@ import { Link } from "react-router-dom";
 import imageMariage from 'assets/img/IconesAccueils/Mariage.png';
 import imageDeces from 'assets/img/IconesAccueils/Deces.png';
 import imageDivorce from 'assets/img/IconesAccueils/Divorce.png';
+import DecisionJustice from "assets/img/IconesAccueils/DecisionJustice.png"
 
 import {
     Container,
     Row,
     Col,
+    Button,
+    Modal,
+    Input,
+    UncontrolledTooltip,
   } from "reactstrap";
 
 
   const TITLE = 'Côte d’Ivoire - Accueil Mairie'
 
 class AccueilMairie extends Component {
-    state = {  }
+
+    constructor(props){
+        super(props);
+        this.state = {
+            modal:false,
+        }
+        console.log(this.state);
+        //this.state = this.state.bind(this)
+    }
+
+    toggleModal(){
+        if (this.state.modal===false){
+            console.log("false");
+            this.setState((prevState) => ({...prevState,modal :true}));
+        }else{
+            console.log("false");
+            this.setState((prevState) => ({...prevState,modal :false}));
+        }        
+    }
+
     render() { 
         return ( 
             <>
@@ -28,6 +52,7 @@ class AccueilMairie extends Component {
                         <h1 className="ml-4" style={{color:"gray"}}>ACCUEIL</h1>
                     </div>
                 </Row> */}
+                
                 <Row style={{paddingTop:"100px"}}>
                         <Col className="mr-auto ml-auto" md="2" sm="3">
                             <Link to={{
@@ -58,6 +83,75 @@ class AccueilMairie extends Component {
                             <p style ={{fontWeight:"bold"}} className="text-center ct-azure">Déclarer un décès</p>
                         </Col>
                 </Row>
+                <Row style={{paddingTop:"80px"}}>
+                        <Col className="mr-auto ml-auto" md="2" sm="3">
+                                <img
+                                alt="..."
+                                className="img-circle img-no-padding img-responsive"
+                                src={DecisionJustice}
+                                onClick={this.toggleModal.bind(this)}
+                                />
+                                <p style ={{fontWeight:"bold"}} className="text-center ct-green">Enregistrer une décision de justice</p>
+                        </Col>
+                </Row>
+                <Modal isOpen={this.state.modal} toggle={() => this.toggleModal.bind(this)}>
+                <div className="modal-header">
+                  <button
+                    aria-label="Close"
+                    className="close"
+                    type="button"
+                    onClick={() => this.toggleModal()}
+                  >
+                    <span aria-hidden={true}>×</span>
+                  </button>
+                  <h5
+                    className="modal-title text-center"
+                    id="exampleModalLabel"
+                  >
+                    Enregistrer une décision de justice
+                  </h5>
+                </div>
+                <div className="modal-body">
+                    <Row className="decision-justice"
+                        id="tooltip392938669"
+                    >
+                        <Input
+                            defaultValue="option1"
+                            id="exampleRadios1"
+                            name="exampleRadios"
+                            type="radio"
+                        />
+                        Reconnaissance de paternité
+                    </Row>
+                    <UncontrolledTooltip
+                        delay={0}
+                        placement="left"
+                        target="tooltip392938669"
+                    >
+                        Fonctionnalité à venir
+                    </UncontrolledTooltip>
+                    <Row className="decision-justice"
+                        id="tooltip392938669"
+                    >
+                        <Input
+                                defaultValue="option1"
+                                id="exampleRadios1"
+                                name="exampleRadios"
+                                type="radio"
+                            />
+                        Adoption
+                    </Row>
+                </div>
+                <div className="modal-footer">
+
+                  <div className="divider" />
+                  <div className="right-side">
+                    <Button onClick={() => this.toggleModal()} className="btn-link" color="danger" type="button">
+                      Annuler
+                    </Button>
+                  </div>
+                </div>
+              </Modal>
             </Container>
             </>
          );
